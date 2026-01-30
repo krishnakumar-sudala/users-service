268f49ef1304288f771240ff87b8f77eb7676b5b
krishnakumar-sudala <sudala.krishnakumar@gmail.com>
2026-01-30 09:32:27 -0500

    added code for server.js and modified app.js a bit

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const authRoutes = require('./modules/auth/routes');
+const userRoutes = require('./modules/users/routes');
 
 const app = express();
 app.use(express.json());
 
 app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
 
 module.exports = app;
-
-const userRoutes = require('./modules/users/routes');
-app.use('/users', userRoutes);
