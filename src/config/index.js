c9c49b2bb7d2eed388f631f32cd30d79b1b00e8b
krishnakumar-sudala <sudala.krishnakumar@gmail.com>
2026-01-30 16:37:38 -0500

    added code for dbconfig in src/config/index.js

---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  db: {
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB
+  }
+};
