ad93d575368115e75ee6a25e0fe1e1081fa24509
krishnakumar-sudala <sudala.krishnakumar@gmail.com>
2026-02-16 12:12:24 -0500

    corrected the JWT config variable issue on jwt.js on restored code

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -2,19 +2,19 @@ const jwt = require('jsonwebtoken');
 const { jwt: jwtConfig } = require('../config');
 
 function signAccessToken(payload) {
-  return jwt.sign(payload, jwt.secret, { expiresIn: '15m' });
+  return jwt.sign(payload, jwtConfig.secret, { expiresIn: '15m' });
 }
 
 function signRefreshToken(payload) {
-  return jwt.sign(payload, jwt.refreshSecret, { expiresIn: '7d' });
+  return jwt.sign(payload, jwtConfigjwt.refreshSecret, { expiresIn: '7d' });
 }
 
 function verifyAccessToken(token) {
-  return jwt.verify(token, jwt.secret);
+  return jwt.verify(token, jwtConfig.secret);
 }
 
 function verifyRefreshToken(token) {
-  return jwt.verify(token, jwt.refreshSecret);
+  return jwt.verify(token, jwtConfig.refreshSecret);
 }
 
 module.exports = {
